1e901b0dce44f9f21e64966061af9c42fbc8cdce
KhertishL <109499710+KhertishL@users.noreply.github.com>
2024-05-08 13:55:19 +0400

    Update lasercat.ino

--- lasercat/lasercat.ino
+++ lasercat/lasercat.ino
@@ -13,10 +13,13 @@ int IRsensorPin = 11;
 
 int LaserPin = 4;
 
-long randomNumber1;
+long randomNumber1; //variable for storing random angle of servo
 long randomNumber2;
 
+bool state = false;
 
+unsigned long time;
+unsigned long period = 10000; 
 
 //Turning servo automatically with laser
 // void automatic_laser(){
@@ -37,14 +40,22 @@ long randomNumber2;
 //   }
 // }
 
+
+
 //Turning servo in random position 
 void random_laser_position(){
-  digitalWrite(LaserPin, HIGH);
-  randomNumber1 = random(0, 180);
-  randomNumber2 = random(0, 180);
-  delay(1000);
-  myservo.write(randomNumber1);
-  myservo2.write(randomNumber2);
+  for(int i = 0; i < 10; i++){
+    digitalWrite(LaserPin, HIGH);
+    randomNumber1 = random(0, 180);
+    randomNumber2 = random(0, 180);
+    delay(1000);
+    myservo.write(randomNumber1);
+    myservo2.write(randomNumber2);
+
+    if(i == 9){
+      food_motor();
+    }
+  }
 }
 
 //Turn off laser and reset servo position
@@ -54,31 +65,52 @@ void turn_off_laser(){
   myservo2.write(0);
 }
 
+//Main function for laser positioning with servo
 void main_laser_function(){
-  int IRvalue = digitalRead(IRsensorPin);
-      Serial.println(IRvalue);
-      
-      if(IRvalue == 0){
-        random_laser_position();
-      }
-      else{
-        turn_off_laser();
-      }
+
+    int IRvalue = digitalRead(IRsensorPin);
+    Serial.println(IRvalue);
+
+    if(IRvalue == 0){
+      random_laser_position();
+    }
+    else{
+      turn_off_laser();
+    }
+
+
 }
+
+//Function to control food dispenser motor
+void food_motor(){
+  for (int pos = 0; pos <= 60; pos += 1) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    myservo3.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15 ms for the servo to reach the position
+  }
+  for (int pos = 60; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+    myservo3.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15 ms for the servo to reach the position
+  }
+}
+
+
   
   
 void setup(){
-    pinMode(IRsensorPin, INPUT);
+    pinMode(IRsensorPin, INPUT); 
     pinMode(LaserPin, OUTPUT);
     myservo.attach(servoPinLaserA);
     myservo2.attach(servoPinLaserB);
     myservo3.attach(servoPinFoodC);
-    randomSeed(analogRead(0));
+    randomSeed(analogRead(0)); //random number generator on analog pin A0
     Serial.begin(9600);
   }
     
 void loop(){
   main_laser_function();
+  Serial.println(time);
+  
+ 
 }
-    
-
+   
